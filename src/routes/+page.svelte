9bcf993d5dcaa296effb94fd1321e828c6a27e95
El Cato <elcato5400@gmail.com>
2024-06-18 20:30:39 +0300

    shadows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,29 +2,32 @@
     <title>الة حاسبة</title>
 </svelte:head>
 
-<!-- padding -->
-<div class="bg-white rounded-3xl grid grid-cols-4 gap-1 p-6">
-    <div class="bg-blue-500 rounded-full col-span-4 h-16 flex items-center px-4 mb-4">
+<div
+    class="bg-white rounded-3xl grid grid-cols-4 gap-1 p-6 font-semibold text-xl shadow-xl"
+>
+    <div
+        class="bg-blue-500 rounded-full col-span-4 h-12 flex items-center px-4 mb-2 text-white"
+    >
         123456
     </div>
-    <button> % </button>
-    <button> √ </button>
-    <button> CE </button>
-    <button> C </button>
+    <button class="bg-[#f3f6fc]"> % </button>
+    <button class="bg-[#f3f6fc]"> √ </button>
+    <button class="bg-[#f3f6fc]"> CE </button>
+    <button class="bg-[#1f2b54] text-white"> C </button>
     <button> 7 </button>
     <button> 8 </button>
     <button> 9 </button>
-    <button> - </button>
+    <button class="bg-[#fd3f59] text-white"> - </button>
     <button> 4 </button>
     <button> 5 </button>
     <button> 6 </button>
-    <button> ÷ </button>
+    <button class="bg-[#2285fd] text-white"> ÷ </button>
     <button> 1 </button>
     <button> 2 </button>
     <button> 3 </button>
-    <button> x </button>
+    <button class="bg-[#f9c80e] text-white"> x </button>
     <button> . </button>
     <button> 0 </button>
     <button> = </button>
-    <button> + </button>
+    <button class="bg-[#63dc74] text-white"> + </button>
 </div>
