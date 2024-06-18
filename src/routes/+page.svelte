1053d708553f2d76f0d238e99bed5829c0aaef37
El Cato <elcato5400@gmail.com>
2024-06-18 21:51:21 +0300

    add numbers to the calculator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,24 @@
-<script>
-    import SquareRootIcon from "$lib/icons/SquareRoot.svelte";
+<script lang="ts">
     import MinusIcon from "$lib/icons/Minus.svelte";
     import DivisionIcon from "$lib/icons/Division.svelte";
     import MultiplyIcon from "$lib/icons/Multiply.svelte";
     import PlusIcon from "$lib/icons/Plus.svelte";
+    import BackSpaceIcon from "$lib/icons/Backspace.svelte";
+
+    function addToEquation(value: string) {
+        equation += value;
+    }
+
+    // TODO: fix remove spaces
+    function backspace() {
+        equation = equation.substring(0, equation.length - 1);
+    }
+
+    function clear() {
+        equation = "";
+    }
+
+    let equation: string = "";
 </script>
 
 <svelte:head>
@@ -13,39 +28,56 @@
 <div
     class="bg-white rounded-3xl grid grid-cols-4 gap-1 p-6 font-semibold text-xl shadow-xl"
 >
+    <!-- TODO: fix overflowing numbers -->
     <div
         class="bg-blue-500 rounded-full col-span-4 h-12 flex items-center px-4 mb-2 text-white"
     >
-        123456
+        {equation}
     </div>
-    <button class="bg-[#f3f6fc]"> % </button>
-    <button class="bg-[#f3f6fc]">
-        <SquareRootIcon />
+    <button on:click={() => addToEquation(" / 100")} class="bg-[#f3f6fc]">
+        %
+    </button>
+    <button on:click={() => backspace()} class="bg-[#f3f6fc]">
+        <BackSpaceIcon />
     </button>
     <button class="bg-[#f3f6fc]"> CE </button>
-    <button class="bg-[#1f2b54] text-white"> C </button>
-    <button> 7 </button>
-    <button> 8 </button>
-    <button> 9 </button>
-    <button class="bg-[#fd3f59] text-white">
+    <button on:click={() => clear()} class="bg-[#1f2b54] text-white">
+        C
+    </button>
+    <button on:click={() => addToEquation("7")}> 7 </button>
+    <button on:click={() => addToEquation("8")}> 8 </button>
+    <button on:click={() => addToEquation("9")}> 9 </button>
+    <button
+        on:click={() => addToEquation(" - ")}
+        class="bg-[#fd3f59] text-white"
+    >
         <MinusIcon />
     </button>
-    <button> 4 </button>
-    <button> 5 </button>
-    <button> 6 </button>
-    <button class="bg-[#2285fd] text-white">
+    <button on:click={() => addToEquation("4")}> 4 </button>
+    <button on:click={() => addToEquation("5")}> 5 </button>
+    <button on:click={() => addToEquation("6")}> 6 </button>
+    <button
+        on:click={() => addToEquation(" / ")}
+        class="bg-[#2285fd] text-white"
+    >
         <DivisionIcon />
     </button>
-    <button> 1 </button>
-    <button> 2 </button>
-    <button> 3 </button>
-    <button class="bg-[#f9c80e] text-white">
+    <button on:click={() => addToEquation("1")}> 1 </button>
+    <button on:click={() => addToEquation("2")}> 2 </button>
+    <button on:click={() => addToEquation("3")}> 3 </button>
+    <button
+        on:click={() => addToEquation(" * ")}
+        class="bg-[#f9c80e] text-white"
+    >
         <MultiplyIcon />
     </button>
-    <button> . </button>
-    <button> 0 </button>
+    <button on:click={() => addToEquation(".")}> . </button>
+    <button on:click={() => addToEquation("0")}> 0 </button>
     <button> = </button>
-    <button class="bg-[#63dc74] text-white">
+    <button
+        on:click={() => addToEquation(" + ")}
+        class="bg-[#63dc74] text-white"
+    >
         <PlusIcon />
     </button>
 </div>
