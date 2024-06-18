6b4266aa33e944642b53ae58f4f8ecd707e790de
El Cato <elcato5400@gmail.com>
2024-06-18 21:21:32 +0300

    font and icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+    import SquareRootIcon from "$lib/icons/SquareRoot.svelte";
+    import MinusIcon from "$lib/icons/Minus.svelte";
+    import DivisionIcon from "$lib/icons/Division.svelte";
+    import MultiplyIcon from "$lib/icons/Multiply.svelte";
+    import PlusIcon from "$lib/icons/Plus.svelte";
+</script>
+
 <svelte:head>
     <title>الة حاسبة</title>
 </svelte:head>
@@ -11,23 +19,33 @@
         123456
     </div>
     <button class="bg-[#f3f6fc]"> % </button>
-    <button class="bg-[#f3f6fc]"> √ </button>
+    <button class="bg-[#f3f6fc]">
+        <SquareRootIcon />
+    </button>
     <button class="bg-[#f3f6fc]"> CE </button>
     <button class="bg-[#1f2b54] text-white"> C </button>
     <button> 7 </button>
     <button> 8 </button>
     <button> 9 </button>
-    <button class="bg-[#fd3f59] text-white"> - </button>
+    <button class="bg-[#fd3f59] text-white">
+        <MinusIcon />
+    </button>
     <button> 4 </button>
     <button> 5 </button>
     <button> 6 </button>
-    <button class="bg-[#2285fd] text-white"> ÷ </button>
+    <button class="bg-[#2285fd] text-white">
+        <DivisionIcon />
+    </button>
     <button> 1 </button>
     <button> 2 </button>
     <button> 3 </button>
-    <button class="bg-[#f9c80e] text-white"> x </button>
+    <button class="bg-[#f9c80e] text-white">
+        <MultiplyIcon />
+    </button>
     <button> . </button>
     <button> 0 </button>
     <button> = </button>
-    <button class="bg-[#63dc74] text-white"> + </button>
+    <button class="bg-[#63dc74] text-white">
+        <PlusIcon />
+    </button>
 </div>
